9e3fff64a4ea0a62e94dfe496c1037fea01bce8c
Acer <sirajmuhd.123@gmail.com>
2024-07-12 22:29:43 +0530

    first commit

---
+++ arrayMethods.js
@@ -0,0 +1,17 @@
+//topic-1 concat
+// example 1
+const arr1 = [1,2,3,4,5]
+const arr2 = [6,7,8,9,10]
+
+const concatExample1 = arr1.concat(arr2)
+
+console.log(concatExample1);
+
+//example 2
+const arr3 = [1,2,3,4,5]
+const arr4 = [6,7,8,9,10]
+const arr5 = [11,12,13,14,15]
+
+const concatExample2 = arr3.concat(arr4,arr5)
+
+console.log(concatExample2);
