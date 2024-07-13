2dce35ebdda8d69dfd05572b87430081d6b21e97
Acer <sirajmuhd.123@gmail.com>
2024-07-13 17:59:59 +0530

    topic 18

--- arrayMethods.js
+++ arrayMethods.js
@@ -94,3 +94,194 @@ const arr21 =arr20.findIndex((arr20)=>{
 console.log(arr21);
 
 ////////////////////////////////////////////////////
+
+// topic-6 flat
+
+//example 1
+const arr22 = [1,[2,3],[4,[5,6]]];
+const arr23 = arr22.flat();
+
+console.log(arr23);
+
+//example 2
+const arr24 = [1,[2,3],[4,[5,6]]]
+const arr25 = arr24.flat(Infinity)
+
+console.log(arr25);
+
+/////////////////////////////////////////////////
+
+//topic-7 includes
+
+//example 1
+const arr26 = [1,2,3,4,5]
+const arr27 = arr26.includes(2)
+
+console.log(arr27);
+
+//example 2
+const arr28 = ['siraj','robin','soloman']
+const arr29 = arr28.includes('kelvin')
+console.log(arr29);
+
+////////////////////////////////////////////////
+
+//topic-8 indexOf
+
+//example 1
+const arr30 = [1,2,3,4,5]
+const arr31 = arr30.indexOf(3)
+
+console.log(arr31);
+
+//example 2
+const arr32 = ['lukhman','naja','reja','nahana']
+const arr33 = arr32.indexOf('naja')
+
+console.log(arr33);
+
+////////////////////////////////////////////////
+
+//topic-9 join
+
+//example 1
+const arr34  = [1,2,3,4,5]
+const arr35 = arr34.join()
+
+console.log(arr35);
+
+//example 2
+const arr36 = ['Hello Kelvin','How are you?']
+const arr37 = arr36.join(', ')
+
+console.log(arr37);
+
+//////////////////////////////////////////////
+
+// topic-10 lastIndexOf
+
+//example 1
+const arr38 = [1,2,3,4,5,1,2,3,4,5]
+const arr39 = arr38.lastIndexOf(3)
+
+console.log(arr39);
+
+//example 2 
+const arr40 = ['siraj','lukhman','ashiq','siraj','irfan','ashiq']
+const arr41 = arr40.lastIndexOf('kelvin')
+
+console.log(arr41);
+
+/////////////////////////////////////////////
+
+//topic-11 pop
+
+//example 1
+const arr42 = [1,2,3,4,5]
+const arr43 = arr42.pop(5)
+
+console.log(arr42);
+
+/////////////////////////////////////////////
+
+//topic-12 push
+
+//example 1
+const arr44 = [1,2,3,4]
+const arr45 = arr44.push(5)
+
+console.log(arr44);
+
+//////////////////////////////////////////////
+
+//topic-13 reverse
+
+//example 1
+const arr46 = [5,4,3,2,1]
+const arr47 = arr46.reverse()
+
+console.log(arr47);
+
+//example 2
+const arr48 =['world','Hello']
+const arr49 = arr48.reverse()
+const arr50 = arr48.join(' ')
+
+console.log(arr50);
+
+//////////////////////////////////////////////
+
+//topic-14 unshift 
+
+//example 1
+const arr51 = [2,3,4,5]
+arr51.unshift(1)
+
+console.log(arr51);
+
+//////////////////////////////////////////////
+
+//topic-15 shift 
+
+//example 1
+const arr52 = [10,1,2,3,4,5]
+arr52.shift(10)
+
+console.log(arr52);
+
+////////////////////////////////////////////
+
+//topic-16 slice
+
+//example 1
+const arrFruits = ['Apple','Orange','Tree','Dog']
+const arrRemoveOthers = arrFruits.slice(0,2)
+
+console.log(arrRemoveOthers);
+
+/////////////////////////////////////////////
+
+//topic-17 some
+
+//example 1
+const arr53 = [1,5,8,13,14,15]
+const check = arr53.some((arr53)=>{
+    return arr53>10
+})
+
+console.log(check);
+
+//example 2
+const arr54 = [1,2,3,4,5]
+const check2 = arr54.some((arr54)=>{
+    return arr54>10
+})
+
+console.log(check2);
+
+//////////////////////////////////////////////
+
+//topic-18 every
+
+//example 1
+const arr55 = [10,11,12,13,14,15]
+const check3 = arr55.every((arr55)=>{
+    return arr55>11
+})
+
+console.log(check3);
+
+//example 2
+const arr56 = [10,11,12,13,14,15]
+const check4 = arr56.every((arr56)=>{
+    return arr56>=10
+})
+
+console.log(check4);
+
+/////////////////////////////////////////////////
+
+//topic-19 sort
+
+//example 1
+
